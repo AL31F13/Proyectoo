efd2cafd014d40aeceab2a2147cf98ec5da7cd26
AL31F13 <laura.Ocelotl.ponce@gmail.com>
2015-06-25 10:58:39 -0500

    UNO
    se subio

---
+++ js/acciones.js
@@ -0,0 +1,15 @@
+
+// JavaScript Document
+$(document).ready(function(e) {
+	document.addEventListener("deviceready",function(){
+
+	
+$ ('#btnvibrar') .on ('tap' , function () { 
+  navigator .vibrate (1000);
+  
+ });//  tap a btnvibrar
+
+
+}); 
+});
+
